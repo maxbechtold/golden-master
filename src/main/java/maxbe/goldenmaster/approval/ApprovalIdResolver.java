70ab894dae1fed165fa145ab01d5d8c84734ead6
Max Bechtold <max.bechtold@andrena.de>
2020-02-03 14:19:22 +0100

    #11 Mark FIXME

--- src/main/java/maxbe/goldenmaster/approval/ApprovalIdResolver.java
+++ src/main/java/maxbe/goldenmaster/approval/ApprovalIdResolver.java
@@ -31,7 +31,8 @@ public class ApprovalIdResolver {
     private String getRunIdSuffix(String displayName) {
         // Sadly, it's infeasible to provide the invocation index via the ExtensionContext as we get here
         // @BeforeEach test invocation. Thus, derive it from the display name.
-        // TODO MAX Perhaps ExtensionContext#getConfigurationParameter can provide the index?
+        // Perhaps ExtensionContext#getConfigurationParameter can provide the index?
+        // FIXME #11 Without multiple invocationContexts, this will not find a name[index] pattern and crash
         String idWithoutBraces = displayName.substring(1, displayName.length() - 1);
         int runId = Integer.valueOf(idWithoutBraces) - 1;
         return "[" + runId + "]";
